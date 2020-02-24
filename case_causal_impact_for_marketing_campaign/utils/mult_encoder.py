2d304c99e1626e639f85baee828669999865ec3c
chrischang <chang730807@gmail.com>
2020-02-24 15:50:15 +0800

    Add files via upload

---
+++ case_causal_impact_for_marketing_campaign/utils/mult_encoder.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+class MultiColumnLabelEncoder:
+    def __init__(self,columns = None):
+        self.columns = columns
+
+    def fit(self,X,y=None):
+        return self
+
+    def transform(self,X):
+        '''
+        transform columns of X specified in self.columns using
+        LabelEncoder(). If no columns specified, transform all
+        columns in X.
+        '''
+        output = X.copy()
+        if self.columns is not None:
+            for col in self.columns:
+                output[col] = LabelEncoder().fit_transform(output[col])
+        else:
+            for colname,col in output.iteritems():
+                output[colname] = LabelEncoder().fit_transform(col)
+        return output
+
+    def fit_transform(self,X,y=None):
+        return self.fit(X,y).transform(X)
